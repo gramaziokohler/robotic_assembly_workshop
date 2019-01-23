26c98c7b0305aebe91070111fb87e8e2e1d47805
Romana Rust <rust@arch.ethz.ch>
2019-01-23 08:48:05 +0100

    Update ex21_load_compas_fab_robot.py

--- examples/ex21_load_compas_fab_robot.py
+++ examples/ex21_load_compas_fab_robot.py
@@ -1,44 +1,26 @@
 import os
-
 import compas
-import compas.robots
-from compas.robots import LocalPackageMeshLoader
-from compas.robots import Joint
-
+from compas.robots import RobotModel
+#from compas.robots import LocalPackageMeshLoader
 from compas_fab.robots import Robot
+from compas_fab.artists import BaseRobotArtist
+#from compas_fab.ghpython import RobotArtist
 from compas_fab.robots import RobotSemantics
 from compas_fab.backends import RosClient
-
-#from compas_fab.ghpython import RobotArtist
 
 compas.PRECISION = '12f'
 
 path = r"C:\Users\rustr\workspace\robot_description"
 package = 'ur_description'
+urdf_filename = os.path.join(path, package, "urdf", "ur5.urdf")
+srdf_filename = os.path.join(path, package, "ur5.srdf")
 
-loader = LocalPackageMeshLoader(path, package)
-
-urdf_filename = "ur5.urdf"
-srdf_filename = "ur5.srdf"
-
-urdf_filename = os.path.join(path, package, "urdf", urdf_filename)
-srdf_filename = os.path.join(path, package, srdf_filename)
-
-model = compas.robots.Robot.from_urdf_file(urdf_filename)
-#model.load_geometry(loader)
-artist = None
+model = RobotModel.from_urdf_file(urdf_filename)
+artist = BaseRobotArtist(model)
 #artist = RobotArtist(model)
 semantics = RobotSemantics.from_srdf_file(srdf_filename, model)
+client = RosClient('127.0.0.1', 9090)
+client.run()
 
-robot = Robot(model, artist, semantics)
-
-print("planning groups:", robot.group_names)
-print("main planning group:", robot.main_group_name)
-print("base_frame:", robot.get_base_frame())
-print("Joints of main planning group:")
-for joint in robot.get_configurable_joints(robot.main_group_name):
-    info = "\t%s is of type '%s'" % (joint.name, list(Joint.SUPPORTED_TYPES)[joint.type])
-    if joint.limit:
-        info += " and has limits [%.3f, %.3f]" % (joint.limit.upper, joint.limit.lower)
-    print(info)
-
+robot = Robot(model, artist, semantics, client)
+robot.info()
