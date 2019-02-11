2f75130bdf50577b93c4f25a6e768fa8c5e82e88
Romana Rust <rust@arch.ethz.ch>
2019-02-11 12:28:36 +0100

    Create ex21_robot_artist.py

---
+++ examples/ex21_robot_artist.py
@@ -0,0 +1,27 @@
+"""
+Creates a robot model of a UR5 robot
+from URDF/SRDF files.
+"""
+import os
+
+from compas.robots import RobotModel
+from compas.robots import LocalPackageMeshLoader
+from compas_fab.robots import Robot
+from compas_fab.rhino import RobotArtist
+
+HERE = os.path.dirname(__file__)
+DATA = os.path.join(HERE, '../data')
+PATH = os.path.join(DATA, 'robot_description')
+
+package = 'ur_description'
+urdf_filename = os.path.join(PATH, package, "urdf", "ur5.urdf")
+
+model = RobotModel.from_urdf_file(urdf_filename)
+
+# Load external geometry files (i.e. meshes)
+loader = LocalPackageMeshLoader(PATH, package)
+model.load_geometry(loader)
+
+artist = RobotArtist(model)
+M = artist.draw_visual()
+print M
