74dd406d9e7e4ec628f3c5a03b167656a3e801bb
Romana Rust <rust@arch.ethz.ch>
2019-02-11 21:52:30 +0100

    add default attributes

--- examples/ex51_transform_assembly_to_workstation.py
+++ examples/ex51_transform_assembly_to_workstation.py
@@ -11,10 +11,13 @@ from compas_assembly.datastructures import assembly_transform
 
 HERE = os.path.dirname(__file__)
 DATA = os.path.join(HERE, '../data')
-PATH_FROM = os.path.join(DATA, '08_wall_interfaces.json')
+PATH_FROM = os.path.join(DATA, '090_wall_courses.json')
 PATH_TO = os.path.join(DATA, '51_wall_transformed.json')
 
 assembly = Assembly.from_json(PATH_FROM)
+
+# Set default attributes which will be changed later on in the planning process
+assembly.update_default_vertex_attributes(is_buildable=False, is_planned=False, is_built=False)
 
 # Define the frame where you want to transform the brick wall to.
 pt = (-0.189775, -0.871978, 0.518383)
